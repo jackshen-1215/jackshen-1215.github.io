f1b3ba477e1cf16afb039b5e91ba7d287b7279e8
jackshen-1215 <jackshen1215@gmail.com>
2025-11-20 22:34:21 +0000

    Deploying to gh-pages from @ jackshen-1215/jackshen-1215.github.io@152e13d3410554448a2fc61a453111f78db9f355 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "Download the full PDF or browse the highlights below.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
